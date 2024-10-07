f499942a77b25090af04360cf54134ce1f102d78
Mishe4ka14 <mishechka.14@yandex.ru>
2024-10-07 21:48:29 +0300

    initial commit

---
+++ src/components/header/header.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import { usePathname } from 'next/navigation';
+
+const Header = () => {
+  const pathname = usePathname();
+
+  const isHomePage = pathname === '/';
+  const isCharacterPage = pathname.startsWith('/details/character');
+  const isSeriesPage = pathname.startsWith('/details/series');
+
+  return (
+    <header>
+      <h1>Вселенная Рик и Морти</h1>
+      {isHomePage && (
+        <p>
+          На этой странице вы можете найти любимых персонажей по имени, расе, полу (если он у них есть) и многим другим параметрам.
+        </p>
+      )}
+      {(isCharacterPage || isSeriesPage) && (
+        <p>
+          На этой странице вы можете больше узнать про {isCharacterPage ? 'персонажа' : 'серию'}.
+        </p>
+      )}
+    </header>
+  );
+};
+
+export default Header;
