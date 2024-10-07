a5b1217d3f81a9b530434a2f09623ca58e95fb55
Mishe4ka14 <mishechka.14@yandex.ru>
2024-10-07 23:30:20 +0300

    add header, routing and styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,25 +2,78 @@
 
 import Header from "@/components/header/header";
 import { useState } from "react";
+import { Input, Select } from 'antd';
+
+const { Option } = Select;
 
 const HomePage = () => {
-
+  const [status, setStatus] = useState('unknown');
+  const [gender, setGender] = useState('unknown');
   const [searchQuery, setSearchQuery] = useState('');
 
-  return(
-    <>
-      <Header/>
+  return (
+    <body>
+  <div className="container">
+      <Header />
       <main>
-        <input
-            type="text"
-            value={searchQuery}
-            onChange={(e) => setSearchQuery(e.target.value)}
-            className="border p-2 w-full rounded-lg"
-            placeholder="Введите имя персонажа..."
-          />
+        <div className="flex flex-col items-center">
+          <div className="flex flex-col items-start">
+            <label className="mb-1 text-white">
+              <h4>Имя персонажа</h4>
+            </label>
+            <Input
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
+              placeholder="Введите имя персонажа..."
+              className="w-[90vw] sm:w-[70vw]"
+            />
+          </div>
+        </div>
+
+        <div className="flex justify-around mt-10">
+          <div className="flex flex-col items-center">
+            <div className="flex flex-col items-start">
+              <label className="mb-1 text-white">
+                <h4>Жив???</h4>
+              </label>
+              <Select
+                value={status}
+                onChange={(value) => setStatus(value)}
+                className="w-[80vw] sm:w-[60vw] md:w-[40vw] lg:w-[30vw]"
+                placeholder="Выберите статус"
+              >
+                <Option value="unknown">Неизвестно</Option>
+                <Option value="alive">Жив</Option>
+                <Option value="dead">Мертв</Option>
+              </Select>
+            </div>
+          </div>
+
+          <div className="flex flex-col items-center">
+            <div className="flex flex-col items-start">
+              <label className="mb-1 text-white">
+                <h4>Пол</h4>
+              </label>
+              <Select
+                value={gender}
+                onChange={(value) => setGender(value)}
+                className="w-[80vw] sm:w-[60vw] md:w-[40vw] lg:w-[30vw]"
+                placeholder="Выберите гендер"
+              >
+                <Option value="unknown">Неизвестно</Option>
+                <Option value="male">Мужчина</Option>
+                <Option value="female">Женщина</Option>
+                <Option value="genderless">Без пола</Option>
+              </Select>
+            </div>  
+          </div>
+        </div>
       </main>
-    </>
-  )
+
+
+  </div>
+  </body>
+  );
 }
 
 export default HomePage;
